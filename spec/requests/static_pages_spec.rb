8e2d25d0c0a9f83d0b85758f1299a52acaed09a9
chrs <christaudt@gmx.net>
2013-11-12 20:02:32 +0100

    added guard and spork

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Die Homepage" do
 
     it "Text in Anfuehrungsstrichen wird ignoriert außer 'Home of StaticPages'" do
@@ -11,7 +13,7 @@ describe "Static pages" do
     
     it "should have the right title" do
   visit '/static_pages/home'
-  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+  expect(page).to have_title("#{base_title} | Home")
 end
 
   end
@@ -24,7 +26,7 @@ describe "Help page" do
     
     it "should have the title help" do
   visit '/static_pages/help'
-  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+  expect(page).to have_title("#{base_title} | Help")
 end
 
     
@@ -39,10 +41,23 @@ describe "Da About page" do
     
     it "should have the title About" do
   visit '/static_pages/about'
-  expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+  expect(page).to have_title("#{base_title} | About")
 end
 
   end
 
+describe "A Contact Page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+    
+    it "should have the title Contact" do
+  visit '/static_pages/contact'
+  expect(page).to have_title("#{base_title} | Contact")
+end
+
+  end
 
 end
